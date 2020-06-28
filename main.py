98e2116a540c630645ab92775595b98a5001c514
iankesh <ankesh76626@gmail.com>
2020-06-28 14:06:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+
+a = 5
+b = "hello"
+print (a)
+print (b)
+
+#creating a square
+ankesh_turtle = turtle.Turtle()
+
+def square ():
+  ankesh_turtle.forward (100)
+  ankesh_turtle.right (90)
+  ankesh_turtle.forward (100)
+  ankesh_turtle.right (90)
+  ankesh_turtle.forward (100)
+  ankesh_turtle.right (90)
+  ankesh_turtle.forward (100)
+
+square()
+ankesh_turtle.forward (200)
+square()
